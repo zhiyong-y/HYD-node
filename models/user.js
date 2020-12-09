904ea5e6faac2fa7e55f5e8329e67058cb86d967
yuzhiyong <15684187702@163.com>
2020-12-09 22:05:28 +0800

    解决了查询不报错，但是不返回数据

--- models/user.js
+++ models/user.js
@@ -5,15 +5,15 @@ let Schema = mongoose.Schema;
 
 // 定义业务功能模型,字段要文档中的一致
 let userSchema = new Schema({
-  name: {
-    type: String,
-    require: true
-  },
-  age: {
-    type: Number,
-    require: true
-  }
+  userId: { type: String, require: true },
+  uclass: { type: String, require: true },
+  name: { type: String, require: true },
+  age: { type: Number, require: true },
+  email: { type: String, require: true },
+  birthday: { type: Date, require: true },
+},{
+  collection: 'user'
 });
 
 // 导出模型（实体）
-module.exports = mongoose.model('test', userSchema);
+module.exports = mongoose.model('User', userSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-// 引入mongoose以及业务实体
-// let mongoose = require('mongoose');
-let test = require('../models/user');
+// 引入业务实体
+let User = require('../models/user');
 
 router.get('/lists', function (req, res, next) {
-  console.info(test)
-  test.find(function (err, doc) {
+  User.find(function (err, doc) {
     console.log(doc)
     if (err) {
       res.json({
